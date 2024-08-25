453107f2e2d6a91deb96bba73745f3ceee2f71dd
AS400JPLPC <laroche.jeanpierre@gmail.com>
2024-08-25 08:20:30 +0200

    maj_20240825-08:20

--- src-zig/testmvzr.zig
+++ src-zig/testmvzr.zig
@@ -10,22 +10,33 @@ const reg = @import("match");
 
 const allocatorPrint = std.heap.page_allocator;
 
+// pub fn isMatch(testval : [] const u8, pattern : [] const u8 ) bool {
+
+    
+//      const maybe_regex = freg.compile(pattern) ;
+//      std.debug.print("\r\nlog {s}  {s}\r\n",.{testval,pattern});
+//      if (maybe_regex) |regex|{
+//          const match1 = regex.match(testval);
+//         if (match1) |m1| {
+//             if (testval.len ==  m1.end) return true;
+//         } else {
+//             return false;
+//         }   
+//      }  
+//      return false ;   
+       
+//  }
+
 pub fn isMatch(testval : [] const u8, pattern : [] const u8 ) bool {
 
     
      const maybe_regex = freg.compile(pattern) ;
-     std.debug.print("\r\nlog {s}  {s}\r\n",.{testval,pattern});
-     if (maybe_regex) |regex|{
-         const match1 = regex.match(testval);
-        if (match1) |m1| {
-            if (testval.len ==  m1.end) return true;
-        } else {
-            return false;
-        }   
-     }  
-     return false ;   
+      if (maybe_regex) |regex|{     return regex.isMatch(testval);} else return false;
+    
        
  }
+
+
 
  
 pub fn isMatchiFixedIso(testval : [] const u8) void { 
@@ -84,12 +95,19 @@ std.debug.print("test emprient memoire",.{});
     std.debug.print("Macth A-Z {} \r\n",.{reg.isMatch("P1","^[A-Z]{1,1}[a-zA-Z0-9]{0,}$")}) ;
     std.debug.print("MVZR  A-Z {} \r\n",.{isMatch("P1","^[A-Z]{1,1}[a-zA-Z0-9]{0,}$")}) ;
 
-    std.debug.print("Macth abc {} \r\n",.{reg.isMatch("pppp-1","^[a-zA-Z]{1,1}[a-zA-Z0-9]{0,}$")}) ;
-    std.debug.print("MVZR abc {} \r\n",.{isMatch("pppp-1","^[a-zA-Z]{1,1)([a-zA-Z0-9]{0,})$")}) ;
+    std.debug.print("Macth pppp-1 {} \r\n",.{reg.isMatch("pppp-1","^[a-zA-Z]{1,1}[a-zA-Z0-9]{0,}$")}) ;
+    std.debug.print("MVZR pppp-1 {} \r\n",.{isMatch("pppp-1","^[a-zA-Z]{1}[a-zA-Z0-9]$")}) ;
 
+    std.debug.print("MVZR pppp-1 error  {} \r\n",.{isMatch("pppp-1","^[a-zA-Z]{1,1}([a-zA-Z0-9]{0,}$)")}) ;
+    std.debug.print("MVZR pppp-1 error  {} \r\n",.{isMatch("pppp-1","^[a-zA-Z]{1,1)([a-zA-Z0-9]{0,})$")}) ;
     
-    std.debug.print("MVZR abc {} \r\n",.{isMatch("p3","^[a-zA-Z]{1}[a-zA-Z0-9]{0,}$")}) ;
+    
+    std.debug.print("MVZR p3 {} \r\n",.{isMatch("p3","^[a-zA-Z]{1}[a-zA-Z0-9]{0,}$")}) ;
 
+    std.debug.print("MVZR pé error {} \r\n",.{isMatch("pé","^[a-zA-Z]{1}[a-zA-Z0-9é]{0,}$")}) ;
+
+    // test only caractère 1 alpha
+    std.debug.print("MVZR péazerty12345 test {} \r\n",.{isMatch("péazerty12345","[a-zA-Z]{1}")}) ;
         
   std.debug.print("Macth digit {} \r\n",.{reg.isMatch(
   "423",
@@ -108,52 +126,52 @@ std.debug.print("test emprient memoire",.{});
   "[+][(][0-9]{2,3}[)][0-9]([-. ]?[0-9]{1,3}){1,4}")}) ;
 
 
-  std.debug.print("Macth tel us{} \r\n",.{reg.isMatch(
-  "+(001)456.123.789.123",
-  "[+][(][0-9]{3}[)][0-9]{3}([-.][0-9]{3}){1,4}")}) ;
+ std.debug.print("Macth tel us{} \r\n",.{reg.isMatch(
+ "+(001)456.123.789.123",
+ "[+][(][0-9]{3}[)][0-9]{3}([-.][0-9]{3}){1,4}")}) ;
 
-  std.debug.print("MVZR tel us{} \r\n",.{isMatch(
-  "+(001)456.123.789.123",
-  "[+][(][0-9]{3}[)][0-9]{3}([-. ]?[0-9]{3}){1,4}")}) ;
+ std.debug.print("MVZR tel us{} \r\n",.{isMatch(
+ "+(001)456.123.789.123",
+ "[+][(][0-9]{3}[)][0-9]{3}([-. ]?[0-9]{3}){1,4}")}) ;
 
  
-  std.debug.print("Macth date fr{} \r\n",.{reg.isMatch(
-  "12/10/1951",
-  "(0[1-9]|[12][0-9]|3[01])[\\/](0[1-9]|1[012])[\\/][0-9]{4}")});
+ std.debug.print("Macth date fr{} \r\n",.{reg.isMatch(
+ "12/10/1951",
+ "(0[1-9]|[12][0-9]|3[01])[\\/](0[1-9]|1[012])[\\/][0-9]{4}")});
  
-  std.debug.print("MVZR date fr{} \r\n",.{isMatch(
-  "12/10/1951",
-  "(0[1-9]|[12][0-9]|3[01])[\\/](0[1-9]|1[012])[\\/][0-9]{4}")});
+ std.debug.print("MVZR date fr{} \r\n",.{isMatch(
+ "12/10/1951",
+ "(0[1-9]|[12][0-9]|3[01])[\\/](0[1-9]|1[012])[\\/][0-9]{4}")});
 
 
-  std.debug.print("Macth date us{} \r\n",.{reg.isMatch(
-  "10/12/1951",
-  "(0[1-9]|1[012])[\\/](0[1-9]|[12][0-9]|3[01])[\\/][0-9]{4}")});
+ std.debug.print("Macth date us{} \r\n",.{reg.isMatch(
+ "10/12/1951",
+ "(0[1-9]|1[012])[\\/](0[1-9]|[12][0-9]|3[01])[\\/][0-9]{4}")});
 
-  std.debug.print("MVZR date us{} \r\n",.{isMatch(
-  "10/12/1951",
-  "(0[1-9]|1[012])[\\/](0[1-9]|[12][0-9]|3[01])[\\/][0-9]{4}")});
-
-
-  std.debug.print("Macth date iso{} \r\n",.{reg.isMatch(
-  "2003-02-25",
-  "[0-9]{4}[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])")});
-
-  std.debug.print("MVZR date iso{} \r\n",.{isMatch(
-  "2003-02-25",
-  "[0-9]{4}[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])")});
+ std.debug.print("MVZR date us{} \r\n",.{isMatch(
+ "10/12/1951",
+ "(0[1-9]|1[012])[\\/](0[1-9]|[12][0-9]|3[01])[\\/][0-9]{4}")});
 
 
-  //oreilly
-  std.debug.print("Macth Mail{} \r\n",.{reg.isMatch(
-  "myname.myfirstname@gmail.com",
-  "^[a-zA-Z0-9_!#$%&'*+\\/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")});
+ std.debug.print("Macth date iso{} \r\n",.{reg.isMatch(
+ "2003-02-25",
+ "[0-9]{4}[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])")});
+
+ std.debug.print("MVZR date iso{} \r\n",.{isMatch(
+ "2003-02-25",
+ "[0-9]{4}[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])")});
 
 
-  // standard mail
-  std.debug.print("MVZR Mail{} \r\n",.{isMatch(
-  "myname.my~f{i}r/st_n'*a`me@gmail.com-ase",
-  "^[a-zA-Z0-9_!#$%&=.-^~|?*+'`{}/]+@([a-zA-Z0-9.-])+$")});
+ //oreilly
+ std.debug.print("Macth Mail{} \r\n",.{reg.isMatch(
+ "myname.myfirstname@gmail.com",
+ "^[a-zA-Z0-9_!#$%&'*+\\/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")});
+
+
+ // standard mail
+ std.debug.print("MVZR Mail{} \r\n",.{isMatch(
+ "myn-ame.my~f{i}r/st_n'*a`me@gmail.com-ase",
+ "^[a-zA-Z0-9_!#$%&=.^~|?*+'`{}/-]+@([a-zA-Z0-9.-])+$")});
 
   
   const width :usize = 5;
@@ -246,7 +264,7 @@ std.debug.print("test emprient memoire",.{});
     isMatchiFixedFr("29/02/1951");
 
 
-    isMatchiFixedUs("02/28/4951");
+    isMatchiFixedUs("02/28/1951");
     isMatchiFixedUs("02/29/1951");
          
   std.debug.print("Macth A-Z {} \r\n",.{reg.isMatch("😀P1😀","^😀[A-Z0-9]{1,2}😀$")}) ;
